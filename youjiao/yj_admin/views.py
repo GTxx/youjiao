20b422c55f8d5f68a72119a9b3a72a5d3b89a392
YangTe1 <505305586@qq.com>
2015-11-23 12:00:11 +0800

    adjust dashboard, add more info

--- youjiao/yj_admin/views.py
+++ youjiao/yj_admin/views.py
@@ -3,11 +3,19 @@ from flask_admin import AdminIndexView, expose
 
 
 class YJHomeView(AuthEditorMixin, AdminIndexView):
-
     @expose()
     def index(self):
         from youjiao.user.models import User
+        from youjiao.user.models import Role
         from youjiao.content.models import Activity
         from youjiao.teach_material.models import Courseware, Book
-        return self.render('yj_admin/dashboard.html', User=User,
-                           Activity=Activity, Courseware=Courseware, Book=Book)
+        from youjiao.content.models import Page
+        from youjiao.content.models import OnlineCourse
+        from youjiao.yj_media.models import Video
+        from youjiao.yj_media.models import Audio
+        from youjiao.yj_media.models import Document
+        from youjiao.photo.models import Photo
+        from youjiao.photo.models import Album
+        return self.render('yj_admin/dashboard.html', User=User, Role=Role, Activity=Activity,
+                           Courseware=Courseware, Book=Book, Page=Page, OnlineCourse=OnlineCourse,
+                           Video=Video, Audio=Audio, Document=Document, Photo=Photo, Album=Album)
